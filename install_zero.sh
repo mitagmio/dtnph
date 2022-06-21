380b8e0f607d641faa0f722fa6f4203cfcccaecf
mitagmio <mitag.mio@gmail.com>
2022-06-21 15:38:39 +0000

    fix install

--- install_zero.sh
+++ install_zero.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Install Python
-echo "# Install Python"
-apt remove python3.8 python3.8-dev python3.8-venv python3.8-distutils python3.8-lib2to3 python3.8-gdbm python3.8-tk python-is-python3  python3-pip -y
-apt update -y && apt upgrade -y
-apt install software-properties-common -y
-add-apt-repository ppa:deadsnakes/ppa
-apt install python3.10 python3.10-dev python3.10-venv python3.10-distutils python3.10-lib2to3 python3.10-gdbm python3.10-tk python-is-python3  python3-pip -y
-curl -sS https://bootstrap.pypa.io/get-pip.py | python
-cp /usr/local/bin/*pip* /usr/bin/
-python -m pip install --upgrade pip
-python --version
-python3 --version
-pip --version
-
 # install Docker, docker-compose
 echo "# install Docker, docker-compose"
 apt-get remove docker docker-engine docker.io containerd runc
@@ -30,6 +16,27 @@ docker --version
 curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 docker-compose --version
+
+# Install Python
+echo "# Install Python"
+apt update -y && apt upgrade -y
+apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa
+apt update -y
+apt remove python3.8 python3.8-dev python3.8-venv python3.8-distutils python3.8-lib2to3 python3.8-gdbm python3.8-tk python-is-python3  python3-pip -y
+sudo apt --fix-missing purge $(dpkg -l | grep 'python3' | awk '{print $2}')
+rm /usr/bin/pip*
+rm /usr/bin/python*
+apt install python3.10 python3.10-dev python3.10-venv python3.10-distutils python3.10-lib2to3 python3.10-gdbm python3.10-tk -y
+update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python 10
+curl -sS https://bootstrap.pypa.io/get-pip.py | python
+cp /usr/local/bin/*pip* /usr/bin/
+python -m pip install --upgrade pip
+python --version
+python3 --version
+pip --version
+pip3 --version
 
 # Init venv
 python3 -m venv dtb_venv
